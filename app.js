34ccabb97e136e365670203fe04b6f9db1e7c2ec
Joel christo mark Y <120724349+joelchristomark@users.noreply.github.com>
2024-01-23 08:28:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function call()
+{
+     img1=document.getElementById("img1");
+     img1.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+     
+     img2=document.getElementById("img2");
+    img2.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+
+}
+
+function recall()
+{
+     img3=document.getElementById("img1");
+    img3.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+
+     img4=document.getElementById("img2");
+    img4.src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+
+}
